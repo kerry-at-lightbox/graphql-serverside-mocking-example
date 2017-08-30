8b68beb4ccd01a1f0fdb316bf359379a61d8b93a
kerry-at-lightbox <kerry@lightbox.co.nz>
2017-08-30 17:38:13 +1200

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+import ChannelsListWithData from './components/ChannelsListWithData';
+
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface,
+} from 'react-apollo';
+
+
+const networkInterface = createNetworkInterface({ uri: 'http://localhost:4000/graphql' });
+
+const client = new ApolloClient({
+  networkInterface,
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div className="App">
+          <div className="navbar">React + GraphQL Tutorial</div>
+          <ChannelsListWithData />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
